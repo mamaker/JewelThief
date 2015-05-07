b3baf7f3e8e6aa7912c75c4a77ce7e5721facf08
mamaker <madhuvasudevan@hotmail.com>
2015-05-07 00:03:29 -0500

    Done

--- JewelThief/Program.cs
+++ JewelThief/Program.cs
@@ -10,6 +10,11 @@ namespace JewelThief
     {
         static void Main(string[] args)
         {
+            Owner owner = new Owner();
+            Safe safe = new Safe();
+            JewelThief jewelThief = new JewelThief();
+            jewelThief.OpenSafe(safe, owner);
+            Console.ReadKey();
         }
     }
 }
